e15a35d0363bafd130e1e513f5c0b3df9982174b
ebraminio <ebrahim@gnu.org>
2025-01-15 11:42:37 +0330

    Update update.py

--- update.py
+++ update.py
@@ -12,50 +12,74 @@ WHERE {
   ?qid wdt:P495 wd:Q794. # Iran (Q794) is country of origin (P495)
   ?qid wdt:P3383 ?x.     # Item has a film poster (P3383)
 }
-''', headers={'Content-Type':'application/sparql-query', 'Accept': 'application/json'})
+''', headers={'Content-Type': 'application/sparql-query', 'Accept': 'application/json'})
 
 def wikidata_items(ids):
     for batch in itertools.batched(ids, 50):
         yield from requests.post(
             'https://www.wikidata.org/w/api.php',
-            {'action': 'wbgetentities', 'format': 'json', 'ids': '|'.join(batch)}
+            {'action': 'wbgetentities',
+             'format': 'json',
+             'ids': '|'.join(batch)}
         ).json()['entities'].values()
         print('fetched 50 items, sleep for 2s', file=sys.stderr)
         time.sleep(2)
     print('finished', file=sys.stderr)
 
-ids = list(sorted((x['qid']['value'].split('entity/')[1] for x in queryResult.json()['results']['bindings']), key=lambda x: int(x[1:])))
-
-result = []
-for item in wikidata_items(ids):
-    result.append({
+films = {
+    item['id']: {
         'id': item['id'],
-        'sitelinks': {site: link['title'] for site, link in item['sitelinks'].items() if site == 'fawiki' or site == 'enwiki'},
-        'labels': {lang: label['value'] for lang, label in item['labels'].items() if lang == 'fa' or lang == 'en'},
-        'date': [x['mainsnak']['datavalue']['value']['time'] for x in item['claims'].get('P577', [])],
-        'imdb': [x['mainsnak']['datavalue']['value'] for x in item['claims'].get('P345', [])],
+        'sitelinks': {
+            site: link['title']
+            for site, link in item['sitelinks'].items()
+            if site == 'fawiki' or site == 'enwiki'
+        },
+        'labels': {
+            lang: label['value']
+            for lang, label in item['labels'].items()
+            if lang == 'fa' or lang == 'en'
+        },
+        'date': [x['mainsnak']['datavalue']['value']['time']
+                 for x in item['claims'].get('P577', [])],
+        'imdb': [x['mainsnak']['datavalue']['value']
+                 for x in item['claims'].get('P345', [])],
         'poster': [{
             'image': poster['mainsnak']['datavalue']['value'],
-            'designer': [x['datavalue']['value']['id'] for x in poster.get('qualifiers', {}).get('P170', {})],
+            'designer': [x['datavalue']['value']['id']
+                         for x in poster.get('qualifiers', {}).get('P170', {})],
         } for poster in item['claims'].get('P3383', [])],
-    })
-
-designers = {
-    item['id']: {lang: label['value'] for lang, label in item['labels'].items() if lang == 'fa' or lang == 'en'}
+        'logo': [x['mainsnak']['datavalue']['value']
+                 for x in item['claims'].get('P154', [])],
+    }
     for item in wikidata_items(
-        ({designer
-          for item in result
-          for poster in item['poster']
-          for designer in poster['designer']})
+        sorted(
+            (x['qid']['value'].split('entity/')[1]
+             for x in queryResult.json()['results']['bindings']),
+            key=lambda x: int(x[1:])
+        )
     )
 }
 
-posters = [
-    {**item,
-     'poster': poster['image'],
-     'designer': {designer: designers[designer] for designer in poster['designer']}}
-    for item in result
-    for poster in item['poster']
-]
+designers = {
+    item['id']: {
+        'id': item['id'],
+        'sitelinks': {
+            site: link['title']
+            for site, link in item['sitelinks'].items()
+            if site == 'fawiki' or site == 'enwiki'
+        },
+        'label': {
+            lang: label['value']
+            for lang, label in item['labels'].items()
+            if lang == 'fa' or lang == 'en'
+        },
+    }
+    for item in wikidata_items(
+        {designer
+         for item in films
+         for poster in item['poster']
+         for designer in poster['designer']}
+    )
+}
 
-print(json.dumps(posters, indent=2, ensure_ascii=False, sort_keys=True))
+print(json.dumps({'films': films, 'designers': designers}, indent=2, ensure_ascii=False))
