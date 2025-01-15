f54c076f8180b3969a3f32d782b45eb2e384fadc
Ebrahim Byagowi <ebrahim@gnu.org>
2025-01-15 15:37:26 +0330

    Update the format

--- update.py
+++ update.py
@@ -39,20 +39,22 @@ films = {
             for lang, label in item['labels'].items()
             if lang == 'fa' or lang == 'en'
         }.items())),
+        'directors': [x['mainsnak']['datavalue']['value']['id']
+                      for x in item['claims'].get('P57', [])],
         'date': [x['mainsnak']['datavalue']['value']['time']
                  for x in item['claims'].get('P577', [])],
         'imdb': [x['mainsnak']['datavalue']['value']
                  for x in item['claims'].get('P345', [])],
-        'poster': [{
+        'posters': [{
             'image': poster['mainsnak']['datavalue']['value'],
             'designer': [x['datavalue']['value']['id']
                          for x in poster.get('qualifiers', {}).get('P170', {})],
         } for poster in item['claims'].get('P3383', [])],
-        'logo': [{
-            'image': poster['mainsnak']['datavalue']['value'],
+        'logos': [{
+            'image': logo['mainsnak']['datavalue']['value'],
             'designer': [x['datavalue']['value']['id']
-                         for x in poster.get('qualifiers', {}).get('P170', {})],
-        } for poster in item['claims'].get('P154', [])],
+                         for x in logo.get('qualifiers', {}).get('P170', {})],
+        } for logo in item['claims'].get('P154', [])],
     }
     for item in wikidata_items(
         x['qid']['value'].split('entity/')[1]
@@ -60,7 +62,8 @@ films = {
     )
 }
 
-designers = {
+# designers and directors
+people = {
     item['id']: {
         'id': item['id'],
         'sitelinks': dict(sorted({
@@ -68,7 +71,7 @@ designers = {
             for site, link in item['sitelinks'].items()
             if site == 'fawiki' or site == 'enwiki'
         }.items())),
-        'label': dict(sorted({
+        'labels': dict(sorted({
             lang: label['value']
             for lang, label in item['labels'].items()
             if lang == 'fa' or lang == 'en'
@@ -76,10 +79,13 @@ designers = {
     }
     for item in wikidata_items(
         {designer
-         for item in films.values()
-         for poster in item['poster']
-         for designer in poster['designer']}
+         for film in films.values()
+         for poster in film['posters'] + film['logos']
+         for designer in poster['designer']} |
+        {director
+         for film in films.values()
+         for director in film['directors']}
     )
 }
 
-print(json.dumps({'films': films, 'designers': designers}, indent=2, ensure_ascii=False))
+print(json.dumps({'films': films, 'people': people}, indent=2, ensure_ascii=False))
